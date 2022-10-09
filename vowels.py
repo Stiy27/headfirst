ab113b9853927963e55c39d16d6b28900241b300
Stiy27 <stiy27@gmail.com>
2022-10-09 18:53:52 -0300

    commiting vowels.py v2

--- vowels.py
+++ vowels.py
@@ -1,6 +1,13 @@
 vowels = ["a", "e", "i", "0", "u"]
 word = "Milliways"
+found = []
 
+#Pega cada letra da palavra
 for letter in word:
+    #Verifica se cada letra está em vogais
     if letter in vowels:
-        print(letter)
+        #Verifica se a letra não existe em found e adiciona
+        if letter not in found:
+            found.append(letter)
+        
+print(found)
