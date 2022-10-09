98508798bb9f30ad8adfb8f722e352699b54e0c4
Stiy27 <stiy27@gmail.com>
2022-10-09 20:23:21 -0300

    commiting vowels.py v3

--- vowels.py
+++ vowels.py
@@ -1,5 +1,6 @@
-vowels = ["a", "e", "i", "0", "u"]
-word = "Milliways"
+# -*- coding: utf-8 -*-
+vowels = ["a", "e", "i", "o", "u"]
+word = input("Forneça uma palavra para procurar por vogais: ")
 found = []
 
 #Pega cada letra da palavra
@@ -9,5 +10,10 @@ for letter in word:
         #Verifica se a letra não existe em found e adiciona
         if letter not in found:
             found.append(letter)
-        
+#Imprime a lista de vogais
 print(found)
+print()
+
+#Imprime as vogais em linhas separadas
+for vowel in found:
+    print(vowel)
