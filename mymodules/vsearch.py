3f39462d721316d2ed2ed9de37cb85e4f615b501
Stiy27 <stiy27@gmail.com>
2022-12-12 21:39:04 -0300

    Commit estiliza;PEP8 com PyCodeStyle em vsearch.py

--- mymodules/vsearch.py
+++ mymodules/vsearch.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 
 # Utilizando Funções com anotações (:str, :int)
-# Declaramos que o argumento (word) é uma string e que a função retorna um conjunto: (-> set)
-def vowel_search(phrase:str) -> set:
+# Declaramos que o argumento (word) é uma stringe
+# que a função retorna um conjunto: (-> set)
+def search4vowels(phrase: str) -> set:
     """ Exibe qualquer vogal encontrada em uma palavra fornecida. """
     vowels = set('aeiou')
-    #uniao => vowels.union(set(word))
-    #diferenca => vowels.difference(set(word))
+    # uniao => vowels.union(set(word))
+    # diferenca => vowels.difference(set(word))
     return vowels.intersection(set(phrase))
 
 
 # O argumento (letters) tem o valor padrão 'aeiou' atribuido.
-def search4letters(phrase:str, letters:str='aeiou') -> set:
+def search4letters(phrase: str, letters: str = 'aeiou') -> set:
     """ Retorna um conjunto de letras encontradas na frase. """
     # Transforam o argumento letters em um objeto conjunto
     return set(letters).intersection(set(phrase))
