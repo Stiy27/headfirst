a5fdfe0f6d195f0d123c861ff932232a49a4ec51
Stiy27 <stiy27@gmail.com>
2022-12-17 17:05:00 -0300

    Commit de vsearch4web.py com dunder name dunder main para a nuvem

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -4,26 +4,28 @@ from vsearch import search4letters
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello() -> str:
-    return '<h1><u>Welcome to search4letter on the Web!</u></h1>'
-
-
-@app.route('/search4', methods = ['GET', 'POST'])
+@app.route('/search4', methods=['GET', 'POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    title = 'Here are your results: '
+    title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
-    
-    return render_template('results.html', the_phrase = phrase,
-                           the_letters = letters, the_results = results, the_title = title)
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results,
+                           the_title=title,)
 
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letter on the web!')
 
 
-app.run(debug=True)
+# Permite executar app.run localmente, mas impede que AythonAnywhere o execute
+# Localmente __name__ será '__main__', na núvem __name será 'vsearch4web' \
+    # o proprio arquivo/módulo importado por PythonAnywhere
+if __name__ == '__main__':
+    app.run(debug=True)
